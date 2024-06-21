94719a0f466ab87df0bd34abcabf21dbda3486a1
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2024-06-20 22:36:16 -0600

    feat: adding the support for long text

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 file = open("text.txt", "r")
 text = file.read()
-text = text.replace('\n',' ').replace('\r',' ')
+text = text.replace('\n',' ').replace('\r',' ').replace("'","´")
+print(text)
 
-os.system(f"say -v Samantha {text}")
+os.system(f"say -v Samantha -o output.aiff \"{text}\"")
