b3244db9019e931083a73bf7faf85c8bdf91679c
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2024-08-08 12:49:33 -0600

    feat: language support

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import os
 file = open("text.txt", "r")
 text = file.read()
 text = text.replace('\n',' ').replace('\r',' ').replace("'","´")
+
+language = text[0:7]
+print(f"Language: {language}")
+print("---")
 print(text)
 
-os.system(f"say -v Samantha -o output.aiff \"{text}\"")
+if language == "English":
+    # English
+    os.system(f"say -v Samantha -o output.aiff \"{text}\"")
+else:
+    # Spanish
+    os.system(f"say -v Samantha -o output.aiff \"{text}\"")
+
+
